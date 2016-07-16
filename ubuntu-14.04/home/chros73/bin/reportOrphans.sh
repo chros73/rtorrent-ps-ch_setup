0c6521964b8b48f79d8396b07200abcff12eded2
chros <chros@chrosGX620>
2016-07-16 19:20:12 +0100

    Modify reports a bit (See #64)

--- ubuntu-14.04/home/chros73/bin/reportOrphans.sh
+++ ubuntu-14.04/home/chros73/bin/reportOrphans.sh
@@ -10,11 +10,11 @@
 REPORTSUBJECT="List of Orphaned torrents"
 
 # text for email at the beginning
-REPORTBODY="There are orphaned torrents, manual interaction is required.\n"
+REPORTBODY="There are orphaned torrents, manual interaction is required."
 
 
-# checking for mounting problems and the existence of rtcontrol util
-if [ ! "$MAILHELPERMOUNTVAL" = true ] && [ -L "$RTCONTROLBIN" ]; then
+# checking for mounting and rtorrent problems and the existence of rtcontrol util
+if [ ! "$MAILHELPERMOUNTVAL" = true ] && [ ! "$MAILHELPERRTSTATUSVAL" = true ] && [ -L "$RTCONTROLBIN" ]; then
     # prepare report and set optional subject and text for potentional email
     prepareReport rtlistOrphans "$REPORTSUBJECT" "$REPORTBODY"
 fi
--- ubuntu-14.04/home/chros73/bin/reportStopped.sh
+++ ubuntu-14.04/home/chros73/bin/reportStopped.sh
@@ -10,11 +10,11 @@
 REPORTSUBJECT="List of Stopped torrents"
 
 # text for email at the beginning
-REPORTBODY="There are stopped torrents, manual interaction is required.\n"
+REPORTBODY="There are stopped torrents, manual interaction is required."
 
 
-# checking for mounting problems and the existence of rtcontrol util
-if [ ! "$MAILHELPERMOUNTVAL" = true ] && [ -L "$RTCONTROLBIN" ]; then
+# checking for mounting and rtorrent problems and the existence of rtcontrol util
+if [ ! "$MAILHELPERMOUNTVAL" = true ] && [ ! "$MAILHELPERRTSTATUSVAL" = true ] && [ -L "$RTCONTROLBIN" ]; then
     # prepare report and set optional subject and text for potentional email
     prepareReport rtlistStopped "$REPORTSUBJECT" "$REPORTBODY"
 fi
